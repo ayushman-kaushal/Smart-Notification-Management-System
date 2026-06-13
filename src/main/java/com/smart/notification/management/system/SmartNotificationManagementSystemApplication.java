45604553cf229145cacc6f1ef7fdf1b66e33662b
ayushman kaushal <akaushal@asite.com>
2026-06-13 21:38:45 +0530

    missed in the previous commit

---
+++ src/main/java/com/smart/notification/management/system/SmartNotificationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smart.notification.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartNotificationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartNotificationManagementSystemApplication.class, args);
+	}
+
+}
